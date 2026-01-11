521408ce9834e25c339a32414719d0f32072ef83
tanishq Chavan <stapdr7eam@gmail.com>
2026-01-11 16:20:41 +0530

    more practice for 11-01-16

---
+++ s.java
@@ -0,0 +1,9 @@
+class s {
+    public static void main(String[] args) {
+        String word = "java";
+
+        for (int i = 0; i < word.length();i++) {
+            System.out.println(word.charAt(i));
+        }
+    }
+}
